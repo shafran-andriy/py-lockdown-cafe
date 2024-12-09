a3bbb5b458c8fd7f1a5386fcb0190d343b2db423
shafran <shafran.andriy@gmail.com>
2024-12-09 20:16:48 +0200

    created all packages clases and methods

---
+++ app/cafe.py
@@ -0,0 +1,27 @@
+import datetime
+
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        vaccine = visitor.get("vaccine", {})
+        expiration_date = vaccine.get("expiration_date")
+
+        if not vaccine:
+            raise NotVaccinatedError(
+                "All friends should be vaccinated")
+        if (not expiration_date
+                or expiration_date < datetime.date.today()):
+            raise OutdatedVaccineError(
+                "All friends should be vaccinated")
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError(
+                "Friends should buy {masks_to_buy} masks")
+
+        return f"Welcome to {self.name}"
